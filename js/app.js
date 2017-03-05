03531e0f212b73e40fb45ea5483efa8144faca89
David <david.diez31@free.fr>
2017-03-05 12:12:24 +0100

    connexion ok

--- js/app.js
+++ js/app.js
@@ -1,6 +1,55 @@
-$(document).ready(function() {
+(function() {
+    var app = {
+        url: null,
+        inputUsername: null,
+        inputPassword: null,
+        color: null,
+        connexion: [],
+        init: function() {
+            console.log("init ok");
+            this.url = "../data/users.json";
+            this.listener();
+        },
+        listener: function() {
+            console.log("listener ok");
+            $('#btnConnex').on('click', function() {
+                app.getUsers(app.url);
+            });
+        },
+        getUsers: function(url) {
+            console.log("getUsers ok");
+            $.ajax({
+                url: url,
+                success: this.connect,
+                error: function() {
+                    if (err) {
+                        console.log(err);
+                    }
+                }
+            })
+        },
+        connect: function(data) {
+            console.log("connect ok");
+            // console.log(data.Users);
+            inputUsername = $("#inputUsername").val();
+            inputPassword = $("#inputPassword").val();
+            app.connexion.push({ username: inputUsername, password: inputPassword });
+            // console.log(app.connexion);
+            $.each(data.Users, function(i) {
+                if (data.Users[i].username === inputUsername && data.Users[i].password === inputPassword) {
+                    // console.log(data.Users[i].color);
+                    app.fontColor(data.Users[i].color);
+                } else {
+                    return;
+                }
+            });
+        },
+        fontColor: function(color) {
+            console.log("fontColor ok");
+            // console.log(color);
+            $("*").css("color", color);
+        }
+    }
+    app.init();
 
-
-
-
-});
+})();
